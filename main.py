1f6d79c9761b1fc1396f66fa860a3b60466b9dbb
kohc123 <134005463+kohc123@users.noreply.github.com>
2023-05-31 21:46:37 +0000

    Added more code

--- main.py
+++ main.py
@@ -6,27 +6,36 @@ print()
 
 difficulty = input("What is the difficulty level you want to play at? \nHere are the options: easy, medium, hard: ").lower()
 URL = URL + "difficulty=" + difficulty + "&"
-print(URL)
+print()
 
 questions = input("How many questions do you want? \nChoose a number between 1 and 10: ")
 URL = URL + "amount=" + str(questions)
-print(URL) 
+
+print() 
+print("_"*67)
+print()
 
 
-def trace(*args):
-  print (*args)
-  pass
-
-trace ("Calling", URL)
 response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
 
-trace ("\nText returned:", response.text)
 
-trace ("\nHere are all the key/value pairs in the JSON response:")
-for key, value in data.items():
-  trace (key, ": ", value)
-
+score = 0
 for result in data["results"]:
   guess = input(result["question"])
+  answer = result["correct_answer"]
+  if guess.upper() == answer.upper():
+    score += 1
+    print("CORRECT")
+    print()
+  else:
+    print("INCORRECT")
+    print()
+
+print("Congrats! You got " + str(score) + "/" + str(questions) + "questions correct!")
+
+
+
+
+
