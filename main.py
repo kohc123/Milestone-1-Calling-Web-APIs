ae6935058a6ea82a2a6018fde5b0536d8cb253e6
kohc123 <134005463+kohc123@users.noreply.github.com>
2023-05-31 20:10:39 +0000

    Change

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
-URL = https://opentdb.com/api.php?
+URL = "https://opentdb.com/api.php?category=9&type=boolean&"
 
 print("Welcome to trivia!")
-input("What is the difficulty level you want to play at?")
+print()
+
+difficulty = input("What is the difficulty level you want to play at? \nHere are the options: easy, medium, hard: ").lower()
+URL = URL + "difficulty=" + difficulty + "&"
+print(URL)
+
+questions = input("How many questions do you want? \nChoose a number between 1 and 10: ")
+URL = URL + "amount=" + str(questions)
+
+print(URL)
+
+
