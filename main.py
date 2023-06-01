74f52a2b1a7b67f4e9739e768a28f6ff5dfcb689
Raystar378 <133701010+Raystar378@users.noreply.github.com>
2023-06-01 21:06:22 +0000

    Made exceptions to the code

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ print("Welcome to trivia!")
 print()
 
 difficulty = input("What is the difficulty level you want to play at? \nHere are the options: easy, medium, hard: ").lower()
+
+while difficulty not in ["easy", "medium", "hard"]:
+  print("Make sure to enter a valid word!")
+  difficulty = input("What is the difficulty level you want to play at? \nHere are the options: easy, medium, hard: ").lower()
+  
 URL = URL + "difficulty=" + difficulty + "&"
 print()
 
-questions = input("How many questions do you want? \nChoose a number between 1 and 10: ")
+
+
+questions = input("How many questions do you want? \nChoose a number between 1 and 6: ")
+while int(questions) > 6 or int(questions)<1:
+  print("That's not a number between 1 and 6!")
+  questions = input("Choose a number between 1 and 6: ")
 URL = URL + "amount=" + str(questions)
 
 print()
 print("Please answer the following questions with true or false.") 
 print("Note: &#039; should be replaced by an apostrophe")
 print("Note: &quot should be replaced by a quotation mark")
-print("&Aring; should be replaced by a letter A")
+print("Note: &Aring; should be replaced by a letter A")
 print("_"*67)
 print()
 
